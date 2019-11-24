675a82cc715032780ffa4907fce22009ccd08b11
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-24 18:32:32 -0500

    Provided state to a functional component using React Hooks

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Sidenav = () => {
+  // Provifing state to Funcitonal component using Hooks
+  const [count, setCount] = useState(0); // -> useState() resturns a StateArray
+  //   Explanation for the above statement
+  //   const stateArray = useState(0);
+  //   const count = stateArray[0];
+  //   const setCount = stateArray[1];
+
+  const increment = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  const decrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  };
   return (
-    <div>
+    <>
       <h1 className="my-4">Shop Name</h1>
       <div className="list-group">
         <a href="#" className="list-group-item">
@@ -15,7 +30,16 @@ const Sidenav = () => {
           Category 3
         </a>
       </div>
-    </div>
+      <div className="">
+        <button onClick={increment} className="btn btn-primary">
+          Increment Number
+        </button>
+        <button onClick={decrement} className="btn btn-primary">
+          Decrement Number
+        </button>
+        <h1>{count}</h1>
+      </div>
+    </>
   );
 };
 export default Sidenav;
