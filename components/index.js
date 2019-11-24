b59d771db66a862bc37a904ff50ece9aa308a661
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-23 21:35:21 -0500

    Simplified the index.js code by creating Reusable components

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Sidenav from "./Sidenav";
+import Carousel from "./Carousel";
+import Movielist from "./Movielist";
+import Footer from "./Footer";
+
+export { Navbar, Sidenav, Carousel, Movielist, Footer };
---
+++ pages/about.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const about = () => {
+  return <div>Welcome to About Page </div>;
+};
+export default about;
