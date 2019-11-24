495b16341e4323ac4a22a6b7a1ed4f4132369889
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-24 16:31:12 -0500

    Added some basic Styling

--- pages/index.js
+++ pages/index.js
@@ -32,20 +32,29 @@ const Home = () => (
 
     <Navbar />
 
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-3">
-          <Sidenav />
-        </div>
-        <div className="col-lg-9">
-          <Carousel />
-          <div className="row">
-            <Movielist />
+    <div className="home-page">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <Sidenav />
+          </div>
+          <div className="col-lg-9">
+            <Carousel />
+            <div className="row">
+              <Movielist />
+            </div>
           </div>
         </div>
       </div>
     </div>
     <Footer />
+    <style jsx>
+      {`
+        .home-page {
+          padding-top: 75px;
+        }
+      `}
+    </style>
   </div>
 );
 
