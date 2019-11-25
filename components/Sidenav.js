f5663838ff9a69f620777851b4f69a2dd59b9b90
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-24 22:21:49 -0500

    Added a fn() to shorten the movie description on the card on the home page

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -4,20 +4,10 @@ const Sidenav = props => {
   return (
     <>
       <h1 className="my-4">{props.appName}</h1>
-      <button onClick={props.clickHandler}></button>
       <div className="list-group">
-        <a href="#" className="list-group-item">
-          Category 1
-        </a>
-        <a href="#" className="list-group-item">
-          Category 2
-        </a>
-        <a href="#" className="list-group-item">
-          Category 3
-        </a>
-      </div>
-      <div className="">
-        <h1>{props.count}</h1>
+        <a href="#" className="list-group-item">Category 1</a>
+        <a href="#" className="list-group-item">Category 2</a>
+        <a href="#" className="list-group-item">Category 3</a>
       </div>
     </>
   );
