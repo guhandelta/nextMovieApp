6da6fbb5d3de32a19d9dc07e3645c3c24e668e39
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-27 18:25:37 -0500

    Completed the movie detail page

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -12,20 +12,19 @@ const Movie = (props) => {
         <div className="container">
             <div className="jumbotron">
                 <h1 className="display-4">{movie.name}</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p className="lead">{movie.description}</p>
                 <hr className="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p>{movie.genre}</p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    {movie.longDesc}
                 </p>
             </div>
-            <p>Some Random Text</p>
         </div>
     )
 }
 
-Movie.getInitialProps = async () => {
-    const movie = await  getMovieById('2')
+Movie.getInitialProps = async ({ query }) => { //context obj from Next.js
+    const movie = await getMovieById(query.id)
     return { movie }
 }
 
