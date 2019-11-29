0fe2bae09ed6b0728ec7fc9fa05246757b0331ea
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-28 19:18:01 -0500

    Resolved the missing ID error by randomly generating a unique id for every new movie added

--- actions/index.js
+++ actions/index.js
@@ -63,7 +63,7 @@ export const getMovies = () => {
 
 export const createMovie = (movie) => {
     return new Promise((resolve, reject) => {
-        // Create/Generate ID for movie
+        movie.id = Math.random().toString(36).substr(2, 7)
         MOVIE_DATA.push(movie)
         setTimeout(() => {
             resolve(MOVIE_DATA) // resolve the updated list of movies
